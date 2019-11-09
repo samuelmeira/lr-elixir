a169e111fd27e35fad8e031835c761f3fec15162
Samuel Meira <slmeira1@outlook.com>
2019-11-09 01:00:31 -0300

    fixed app port

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :app, AppWeb.Endpoint,
-  http: [port: 4040],
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
